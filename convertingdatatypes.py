83b9e2afda65c23c46208c277b6133b3dc4b088a
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:04 -0700

    exported from Skill Struck

---
+++ convertingdatatypes.py
@@ -0,0 +1,11 @@
+integer = 8
+Float = 8.0
+string = '8'
+
+conversion = int(Float)
+stringconversion = int(string)
+floatconversion = float(integer)
+
+print(conversion)
+print(stringconversion)
+print(floatconversion)
