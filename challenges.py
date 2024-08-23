4122b2b0ce07949b9c8bd5deadf76ad52fea0379
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:12 -0700

    exported from Skill Struck

---
+++ challenges.py
@@ -0,0 +1,7 @@
+height = input("What is your height?")
+if int(height) >= 48:
+    print("You can ride the roller coaster.")
+elif height == 48:
+    print("You can ride the roller coaster.")
+else:
+    print("You can't ride the roller coaster.")
