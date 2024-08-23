18c958b1409b2b56778d9c735034b01c413dd533
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:21 -0700

    exported from Skill Struck

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+mountains = {
+    "Timpanogos" : 8,
+    "Everest" : 12,
+    "Kilimanjaro" : 10,
+    "Vesuvius" : 11
+}
+
+print(mountains)
+print(mountains["Vesuvius"])
+mountains["Kilimanjaro"] = 15
