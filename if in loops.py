88bacb8351942d7e3cb015e10f7a72ff8bbade2e
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:11 -0700

    exported from Skill Struck

---
+++ if in loops.py
@@ -0,0 +1,10 @@
+foods = ['mushrooms', 'brocoli', 'fish']
+for x in foods:
+    if x == 'mushrooms':
+        print(foods + " are gross")
+    elif x == 'brocoli':
+        print(foods + " is gross")
+    elif x == 'fish':
+        print(foods + " is gross")
+    else:
+        print(input + " is gross")
