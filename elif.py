d6053b1829dcccc001f834d4163a6d67644c5050
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:10 -0700

    exported from Skill Struck

---
+++ elif.py
@@ -0,0 +1,7 @@
+coins = 10
+if coins > 20:
+    print('You have more than enough to buy a puppy')
+elif coins == 20:
+    print('You have exactly enough to buy a puppy')
+else:
+    print('You do not have enough to buy a puppy')
