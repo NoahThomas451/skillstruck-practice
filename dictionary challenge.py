d6c32161bc0caa27594d10325b31cbda41ed7b30
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:23 -0700

    exported from Skill Struck

---
+++ dictionary challenge.py
@@ -0,0 +1,11 @@
+dictionary = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3,
+    'd' : 4,
+    'e' : 5
+}
+
+List = ['list', 'tuple', 'dictionary', 'string', 'integer']
+print(dictionary)
+print(List)
