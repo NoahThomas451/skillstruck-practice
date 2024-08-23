c031bf6b1f1ff98aa52acbf2536ff20600bd4a69
NoahThomas451 <noahbthomas@icloud.com>
2024-08-23 09:25:20 -0700

    exported from Skill Struck

---
+++ multiple parameters.py
@@ -0,0 +1,6 @@
+def variable(first, second):
+    print("Your first choice for a birthday gift would be " + first)
+    print("Your second choice for a birthday gift would be " + second)
+
+variable("bike", "basketball")
+variable("speaker", "tickets")
