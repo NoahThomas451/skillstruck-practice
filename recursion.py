0429057eb01c2c92b9df416f3fe8c11f976510df
NoahThomas451 <noahbthomas@icloud.com>
2024-09-20 10:14:53 -0700

    exported from Skill Struck

---
+++ recursion.py
@@ -0,0 +1,16 @@
+houses = ["Ahmed's house", "Clark's house", "Xin Li's houses", "Melissa's houses"]
+
+def deliver_presents_recursively(houses):
+    if len(houses) == 1:
+        house = houses[0]
+        print("Delivering presents to " + house)
+
+    else:
+        mid = len(houses) // 2
+        first_half = houses[:mid]
+        second_half = houses[mid:]
+
+        deliver_presents_recursively(first_half)
+        deliver_presents_recursively(second_half)
+
+deliver_presents_recursively(houses)
