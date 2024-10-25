e68c67adc9daf24bd31c21b578f9a39ebcda1735
NoahThomas451 <noahbthomas@icloud.com>
2024-10-25 10:06:08 -0700

    exported from Skill Struck

---
+++ Error_Handling.py
@@ -0,0 +1,9 @@
+greeting = 'Hello There'
+try:
+    print(greeting)
+except:
+    print('A problem has occurred')
+else:
+    print('The code worked without a problem')
+finally:
+    print('Your exception handling is complete')
