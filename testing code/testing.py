dfdc864b365cde1e2d2aab7e4faf218efbc7b86b
NoahThomas451 <noahbthomas@icloud.com>
2024-10-25 10:06:00 -0700

    exported from Skill Struck

--- testing code/testing.py
+++ testing code/testing.py
@@ -1,1 +1,7 @@
-print(round(1.855, 2))
+def to_weird_case(words):
+    empty = ""
+    for words in range(len(words)):
+        if words.index() % 2 == 0:
+            return empty.join(words.upper())
+        elif words.index() % 2 == 1:
+            return empty.join(words.lower)
