011a9dc5a6ef6aa329d94f1ae086c5eb2a131a91
NoahThomas451 <noahbthomas@icloud.com>
2024-10-25 10:06:07 -0700

    exported from Skill Struck

---
+++ Different_concatenating_approaches.py
@@ -0,0 +1,6 @@
+name = 'Noah'
+dogs = '0'
+location = 'Arizona'
+print('My name is ' + name + ' and I have ' + dogs + ' dogs and live in ' + location)
+print(f'My name is {name} and I have {dogs} dogs and live in {location}')
+print('My name is', name, 'and I have', dogs, 'dogs and live in', location)
